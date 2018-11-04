80fe37fc02f9754e7b9387dd16554cefb68a59cb
Stuart Fooks <sb.fooks@gmail.com>
2018-11-04 19:45:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,12 +7,22 @@ end
 
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
-top_row_win = [0, 1, 2]
-top_row_win = [0, 1, 2]
+WIN_COMBINATIONS =  [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 8, 8], 
+  [0, 4, 8],
+  [2, 4, 6], 
+  [0, 3, 6], 
+  [1, 4, 8], 
+  [2, 5, 8]
+  ]
 
-if
-  board[top_row_win[0]] == "X" &&
-  board[top_row_win[1]] == "X" &&
-  board[top_row_win[2]] == "X"
-  "X won in the top row"
+def won?[board]
+  WIN_COMBINATIONS.each do |arr|
+    if arr[0] == arr[1] && arr[1] == arr[2]
+      return true
+    else
+      return false
+    end
 end
