844b180a719ea2b0da4c0a552c2cc55fb42060db
Stuart Fooks <sb.fooks@gmail.com>
2018-11-04 19:36:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,11 @@ end
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
 top_row_win = [0, 1, 2]
+top_row_win = [0, 1, 2]
 
 if
   board[top_row_win[0]] == "X" &&
   board[top_row_win[1]] == "X" &&
   board[top_row_win[2]] == "X"
+  "X won in the top row"
+end
