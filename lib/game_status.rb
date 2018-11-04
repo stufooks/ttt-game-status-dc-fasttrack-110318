4a1075f49cf4d5f8382a016dbf54cd97db13a52b
Stuart Fooks <sb.fooks@gmail.com>
2018-11-04 19:35:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,4 +5,11 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-WIN_COMBINATIONS
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
+top_row_win = [0, 1, 2]
+
+if
+  board[top_row_win[0]] == "X" &&
+  board[top_row_win[1]] == "X" &&
+  board[top_row_win[2]] == "X"
